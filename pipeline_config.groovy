77023835aa7af90d6685a9e668beb7cad5cab887
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-22 09:37:04 -0400

    adding default step for build source.  for usage for sonarqube

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,3 +1,18 @@
 keywords{
   message = "Starting My Pipeline, with governance"
 }
+
+steps{
+  build_source{
+    stage = "Build Code"
+    image = "gradle:jdk8"
+    command = "sh './gradlew clean build'"
+    stash{
+      name = "build-results"
+      includes = "./build"
+      excludes = "./src"
+      useDefaultExcludes = false
+      allowEmpty = true
+    }
+  }
+}
