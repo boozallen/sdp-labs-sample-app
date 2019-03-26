35b179b5df069c1a91a5be882f164747574e3663
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 21:09:12 -0400

    removing default step

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,17 +4,17 @@ keywords{
 
 pipeline_template = "standard"
 
-steps{
-  build_source{
-    stage = "Build Code"
-    image = "gradle:jdk8"
-    command = "sh 'gradle clean build'"
-    stash{
-      name = "build-results"
-      includes = "./build"
-      excludes = "./src"
-      useDefaultExcludes = false
-      allowEmpty = true
-    }
-  }
-}
+//steps{
+//  build_source{
+//    stage = "Build Code"
+//    image = "gradle:jdk8"
+//    command = "sh 'gradle clean build'"
+//    stash{
+//      name = "build-results"
+//      includes = "./build"
+//      excludes = "./src"
+//      useDefaultExcludes = false
+//      allowEmpty = true
+//    }
+//  }
+//}
