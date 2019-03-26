bf998071aa1d118c4338380b66d2c62ef7fdccba
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 21:28:51 -0400

    removing failed groovy default step implementation

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,10 +4,17 @@ keywords{
 
 pipeline_template = "standard"
 
-steps{
-  build_source2{
-    stage = "Build Code"
-    image = "gradle:jdk8"
-    script = "./script/build_source.groovy"
-  }
-}
+//steps{
+//  build_source{
+//    stage = "Build Code"
+//    image = "gradle:jdk8"
+//    script = "sh 'gradle clean build'"
+//    stash{
+//      name = "build-results"
+//      includes = "./build"
+//      excludes = "./src"
+//      useDefaultExcludes = false
+//      allowEmpty = true
+//    }
+//  }
+//}
