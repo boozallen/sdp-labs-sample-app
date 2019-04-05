2d7bb200e05bf9e8e97cac237d958a5d0f1de320
Carlos OKieffe <okieffe_carlos@bah.com>
2019-04-04 23:50:05 -0400

    implementing smoke_local

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,5 +1,20 @@
 keywords{
-  message = "Smoke testing  with governance"
+  message = "Smoke testing with governance"
 }
 
 pipeline_template = "smoke"
+
+steps{
+  smoke_local{
+    stage = "Smoke Local"
+    image = "gradle:jdk8"
+    command = "echo 'smoke'"
+    stash{
+      name = "smoke"
+//      includes = "./build"
+//      excludes = "./src"
+      useDefaultExcludes = false
+      allowEmpty = true
+    }
+  }
+}
